1ade2082ae89e556c422510d1e937d654d562eb5
Nabin Sapkota <nawinssap123@gmail.com>
2023-02-13 10:29:54 +0545

    Initial Commit

---
+++ Bio.java
@@ -0,0 +1,9 @@
+public class Bio {
+    public static void main(String[] args) {
+        System.out.println("Name: Nabin Sapkota");
+        System.out.println("Age: 22");
+        System.out.println("Address: kalopul");
+        System.out.println("Education: BIT");
+
+    }
+}
